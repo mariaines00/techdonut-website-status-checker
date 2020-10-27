c08309c303de90348c693f3de81cde19dc610142
Maria Inês Serra <maria.serra@talkdesk.com>
2020-10-27 14:44:49 +0000

    Third - create chan to comunicate between routines

--- main.go
+++ main.go
@@ -20,18 +20,24 @@ func main() {
 		"https://mockymonkey.herokuapp.com/",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link) //show diagram
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c) //show why this is needed to 'pause' main routine
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	resp, _ := http.Head(link)
 
 	if resp.StatusCode >= 500 {
+		c <- "From channel: might be down"
 		fmt.Println(link, "might be down :(")
 		return
 	}
 
 	fmt.Println(link, "is up :)")
+	c <- "From channel:yes it's up"
 }
